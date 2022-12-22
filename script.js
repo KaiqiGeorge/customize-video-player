bcaec26ebc98fc7b6d0a1e1e888bf4ada4e1aaae
KaiqiGeorge <oliviakaiqihuang@gmail.com>
2022-12-21 19:37:16 -0700

    wrote the function for progress bar, volume, playbackrate etc

--- script.js
+++ script.js
@@ -19,17 +19,36 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
-function handleUpdateRange(){
-    video[this.name]=this.value;
+function handleUpdateRange() {
+  video[this.name] = this.value;
 }
 
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
 
+function updatePlayTime(e) {
+    let newTime=(e.offsetX/progress.offsetWidth)*video.duration;
+    video.currentTime=newTime;
+
+}
 
 video.addEventListener("click", togglePlay);
+video.addEventListener("timeupdate", handleProgress);
+video.addEventListener("click", updateIcon);
 toggle.addEventListener("click", togglePlay);
 toggle.addEventListener("click", updateIcon);
 
-skipButtons.forEach(button => button.addEventListener("click", skip));
-ranges.forEach(range=>range.addEventListener("mousemove",handleUpdateRange));
-ranges.forEach(range=>range.addEventListener("change",handleUpdateRange));
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleUpdateRange)
+);
+ranges.forEach((range) => range.addEventListener("change", handleUpdateRange));
+
+let mousedown = false;
+progress.addEventListener("click", updatePlayTime);
+progress.addEventListener("mouseup", ()=>mousedown=false);
+progress.addEventListener("mousedown", ()=>mousedown=true);
+progress.addEventListener("mousemove", (e)=>mousedown && updatePlayTime(e));
 
