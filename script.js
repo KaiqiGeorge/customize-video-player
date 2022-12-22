9cad04638595b4d3f4927273b944276f5f345572
KaiqiGeorge <oliviakaiqihuang@gmail.com>
2022-12-21 19:39:59 -0700

    fix a typo

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const player = document.querySelector(".player");
 const video = player.querySelector(".viewer");
 const progress = player.querySelector(".progress");
-const progressBar = player.querySelector(".progress__filled");
+const progressBar = player.querySelector(".progress_filled");
 const toggle = player.querySelector(".toggle");
 const skipButtons = player.querySelectorAll("[data-skip]");
-const ranges = player.querySelectorAll(".player__slider");
+const ranges = player.querySelectorAll(".player_slider");
 
 function togglePlay() {
   const method = video.paused ? "play" : "pause";
